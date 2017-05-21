fd503a760116482e7f284c603d8e552230f0d419
robson <montenegrodr@gmail.com>
2017-05-21 19:59:05 +0100

    Add all resources to create woud count chart

---
+++ runme.py
@@ -0,0 +1,39 @@
+import csv
+import unicodedata
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+from nltk.corpus import stopwords
+
+all_stopwords = stopwords.words('portuguese')
+
+def remove_accents(input_str):
+    nfkd_form = unicodedata.normalize('NFKD', input_str)
+    only_ascii = nfkd_form.encode('ASCII', 'ignore')
+    return only_ascii
+
+def preprocess(s):
+    s = s.lower().replace('&quot;','')
+    s = s.decode('utf-8')
+    s = remove_accents(s)
+    s = [word for word in s.split() if word not in all_stopwords]
+    s = ' '.join(s)
+    return s
+
+text = ''
+
+with open('./notas.csv', 'r') as f:
+    reader = csv.reader(f)
+    for c, row in enumerate(reader):
+        if c == 0:
+            continue
+        text += preprocess(row[1]) + '\n'
+
+
+# Generate a word cloud image
+wordcloud = WordCloud().generate(text)
+
+# Display the generated image:
+# the matplotlib way:
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
